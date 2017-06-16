4aa77325cdbf09bc7c0de4b66907c761d5455d2e
Jorge Covarrubias <artnhector@gmail.com>
2017-06-16 11:49:12 -0500

    Added Clear Fields Function, Changed Error messages and added WhiteSpace

--- StudentCreation.ps1
+++ StudentCreation.ps1
@@ -1,46 +1,48 @@
-﻿# This line adds the .Net Framework WPF to Powershell that makes the GUI work
+﻿#This line adds the .Net Framework WPF to Powershell that makes the GUI work
 Add-Type -AssemblyName PresentationFramework
+
+#Clear field Function
+Function Clear-Fields(){
+		$userFirst.text=""
+		$userLast.text=""
+		$gradYear.text=""
+		$id.text=""
+}
 
 #Function being called from the button add_Click with its variables 
 Function Create-User($userF,$userL,$gradYr,$pass){
-$lowerName = "$userF$userL".ToLower()
-$fullName = "$userF $userL"
-$email = "$lowerName"+"@<DOMAIN>.us"
-$script = "<BATFILE>.bat"
-$OUName = "Class of $gradYr"
-$OUPath = "ou=$OUName,ou=Students,dc=<DOMAIN>,dc=local"
-$homePath = "\\<SERVER>\Students\$gradYr\$lowerName"
-$pwd = ConvertTo-SecureString -String "$pass" -AsPlainText -force
+		$lowerName = "$userF$userL".ToLower()
+		$SAMAccountname = $lowerName
+		$fullName = "$userF $userL"
+		$email = "$lowerName"+"@<DOMAIN>.us"
+		$script = "<BATFILE>.bat"
+		$OUName = "Class of $gradYr"
+		$OUPath = "ou=$OUName,ou=Students,dc=<DOMAIN>,dc=local"
+		$homePath = "\\<SERVER>\Students\$gradYr\$lowerName"
+		$pwd = ConvertTo-SecureString -String "$pass" -AsPlainText -force
 
-#Checking to see if account exist
-$SAMAccountname = $lowerName
+#Checking to see if account and OU exist
 If(@(Get-ADObject -Filter { SAMAccountname -eq $SAMAccountname }).Count -ge 1){
-[System.Windows.Messagebox]::Show("The object/user $SAMAccountname already exsists. Exiting now...", "Account exists")
-$userFirst.text=""
-$userLast.text=""
-$gradYear.text=""
-$id.text=""
-$result.Text += "An account with the name `"$SAMAccountname`" already exist. `n`n"
-return;
-}ElseIf(@(Get-ADOrganizationalUnit -Filter "Name -like '$OUName'").Count -eq 0){
-[System.Windows.Messagebox]::Show("The OU doesn't exit. Make sure you have the right Graduating Year.", "OU doesn't exists")
-$userFirst.text=""
-$userLast.text=""
-$gradYear.text=""
-$id.text=""
-$result.Text += "Graduating year was outside of scope for `"$SAMAccountname`", please check the year. `n`n"
-return;
-}else{
+		#[System.Windows.Messagebox]::Show("An account with the name `"$SAMAccountname`" already exist. Starting over...", "ERROR: Account exists")
+		$result.Text += "---ERROR---`nAn account with the name `"$SAMAccountname`" already exist. `n`n"
+		Clear-Fields
+	}ElseIf(@(Get-ADOrganizationalUnit -Filter "Name -like '$OUName'").Count -eq 0){
+		#[System.Windows.Messagebox]::Show("The OU doesn't exit. Make sure the OU exist or the graduating year is correct.", "ERROR: OU doesn't exists")
+		$result.Text += "---ERROR---`nThe OU doesn't exit. Make sure the OU exist or the graduating year is correct for `"$SAMAccountname`". `n`n"
+		$gradYear.text=""
+	}Else{
 #Running command
-New-ADUser -Name "$fullName" -SamAccountName "$lowerName" `
- -GivenName "$userF" -Surname "$userL" -DisplayName "$fullName" -UserPrincipalName "$email" `
- -Path "$OUPath" -Enabled $true -AccountPassword $pwd `
- -ChangePasswordAtLogon $false -PasswordNeverExpires $true -CannotChangePassword $true `
- -Description "Student$gradYr" -EmailAddress "$email" -Title "Student" -Department "$gradYr" -Company "$pass" `
- -ScriptPath "$gradYr$Script" -HomeDrive "H:" -HomeDirectory "$homePath" `
- -OtherAttributes @{mailNickname="$pass"}
-}
-$result.Text += "An account for $fullname has been created:`nU: $email`nP: $pass `n`n"
+		New-ADUser -Name "$fullName" -SamAccountName "$lowerName" `
+		-GivenName "$userF" -Surname "$userL" -DisplayName "$fullName" -UserPrincipalName "$email" `
+		-Path "$OUPath" -Enabled $true -AccountPassword $pwd `
+		-ChangePasswordAtLogon $false -PasswordNeverExpires $true -CannotChangePassword $true `
+		-Description "Student$gradYr" -EmailAddress "$email" -Title "Student" -Department "$gradYr" -Company "$pass" `
+		-ScriptPath "$gradYr$Script" -HomeDrive "H:" -HomeDirectory "$homePath" `
+		-OtherAttributes @{mailNickname="$pass"}
+
+		$result.Text += "An account for $fullname has been successfully created:`nU: $email`nP: $pass `n`n"
+		Clear-Fields
+	}
 }
 
 
@@ -76,30 +78,26 @@ $create = $Win.FindName("CreateAccount")
 
 #On click, we will take our variables from the GUI, make them into text and run them by the function Create-User
 $create.Add_Click({
-$userF = $userFirst.Text
-$userL = $userLast.Text
-$gradYr = $gradYear.Text
-$pass = $id.Text
-$gradYrValue = $gradYr -as [Double]
-$gradok = $gradYrValue -ne $NULL
-$passValue = $pass -as [Double]
-$passok = $passValue -ne $NULL
+		$userF = $userFirst.Text
+		$userL = $userLast.Text
+		$gradYr = $gradYear.Text
+		$pass = $id.Text
+		$gradYrValue = $gradYr -as [Double]
+		$gradok = $gradYrValue -ne $NULL
+		$passValue = $pass -as [Double]
+		$passok = $passValue -ne $NULL
 If($userF -eq "" -or $userL -eq "" -or $gradYr -eq "" -or $pass -eq ""){
-[System.Windows.Messagebox]::Show("Please make sure all values are entered correctly", "Missing Values")
-}ElseIf( -not $gradok){
-[System.Windows.Messagebox]::Show("Graduation year must be a numeric value (no letters)", "Graduation Numeric Value Error")
-}ElseIf($gradYr.length -notmatch 4){
-[System.Windows.Messagebox]::Show("Graduation year must be 4 digits long", "Graduation: Four Digit Value")
-}ElseIf( -not $passok){
-[System.Windows.Messagebox]::Show("ID Number must be a numeric value (no letters)", "ID Numeric Value Error")
-}ElseIf($pass.length -notmatch 5){
-[System.Windows.Messagebox]::Show("ID number must be 5 digits long", "ID: Five Digit Value")
-}else{
-Create-User $userF $userL $gradYr $pass
-$userFirst.text=""
-$userLast.text=""
-$gradYear.text=""
-$id.text=""
-}
+		[System.Windows.Messagebox]::Show("Please make sure all values are entered correctly", "ERROR: Missing Values")
+	}ElseIf( -not $gradok){
+		[System.Windows.Messagebox]::Show("Graduation year must be a numeric value (no letters)", "ERROR: Graduation Numeric Value")
+	}ElseIf($gradYr.length -notmatch 4){
+		[System.Windows.Messagebox]::Show("Graduation year must be 4 digits long", "ERROR: Graduation: Four Digit Value")
+	}ElseIf( -not $passok){
+		[System.Windows.Messagebox]::Show("ID Number must be a numeric value (no letters)", "ERROR: ID Numeric Value")
+	}ElseIf($pass.length -notmatch 5){
+		[System.Windows.Messagebox]::Show("ID number must be 5 digits long", "ERROR: ID: Five Digit Value")
+	}Else{
+		Create-User $userF $userL $gradYr $pass
+	}
 })
 $Win.ShowDialog()
